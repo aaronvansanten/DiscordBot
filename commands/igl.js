475b0a8fc22c9f86b23e3bd82e465d74193bd151
aaronvansanten <80952023+aaronvansanten@users.noreply.github.com>
2022-06-13 22:10:16 +0200

    Gitignore file added

--- commands/igl.js
+++ commands/igl.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "igl",
     description: "sets the igl for the game.",
     execute(message, args) {
-        const names = new Array("Olaf", "Sander", "Joran", "Aaron", "Job")
+        const names = new Array("Olaf", "Aaron", "Job")
         var name = names[Math.floor(Math.random()*names.length)];
         message.channel.send("The in game leader for this game is " + name);
     }
