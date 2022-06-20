8b20384626de2b5afa6078795484f29aaaa67f58
aaronvansanten <80952023+aaronvansanten@users.noreply.github.com>
2022-06-20 15:24:34 +0200

    Updated jumbo command

--- commands/jumbo.js
+++ commands/jumbo.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "jumbo",
     description: "Send the jumbo image.",
     execute(message, args) {
-        const images = "/Users/aaron/Documents/Jumbo Logo.jpg";
+        const images = "./Jumbo Logo.jpg";
         message.channel.send({ files: [{ attachment: images, name: 'jumbo.png' }] });
     }
 }
